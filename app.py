7334ad7e9ef287cdc3a32b30d17795e0a9647d03
Vedaang Sharma <ved-win@protonmail.com>
2021-05-08 09:19:24 +0530

    All functions Completed

--- app.py
+++ app.py
@@ -74,6 +74,89 @@ def service_add():
     icaq=input('ICAQ Number:')
     old_service=input('Previous Service Date:')
     service_date=input('Date of Service:')
-    
+    new_service=input('Next Service Date:')
+    ct=input('Type of Check:')
+    tec=input('Assigned Technician:')
+    eng=input('Engiineer in charge:')
+    register_num.append(reg_no)
+    manufactur_type.append(manu_type)
+    manufactur_name.append(manu_name)
+    atd_iata.append(iata)
+    atd_icaq.append(icaq)
+    prev_service.append(old_service)
+    next_service.append(new_service)
+    date.append(service_date)
+    check_type.append(ct)
+    technician.append(tec)
+    engineer.append(eng)
+    print('Service Created Suuccesfully.')
+def login():
+    un=input('Please enter your username/employee id:')
+    if un in technician_id:
+        print('You are logged in as Technician.')
+        
+    elif un in engineer_id:
+        print('You are logged in as Engineer.')
+        
+    elif un in admin_name:
+        pw=input('Please enter your password:')
+        no=admin_name.index(un)
+        if admin_pass[no]==pw:
+            print('You are logged in as Admin.')
+            
+        else:
+            print('Incorrect Password.')
+    else:
+        print('User not found.')
+
+
+
+
+print('Welcome to Aircraft Service Manegement Portal.')
+login()
+while 1<10:
+    cmd=input('Press U to access user manager & S to access service manager:')
+    if "U" in cmd:
+        print('User Manager-')
+        task=input('Press M to modify users & V to view users:')
+        if "M" in task:
+            t1=input('Press A to add users & D to delete users.')
+            if "A" in t1:
+                user_add()
+            elif "D" in t1:
+                user_del()
+            else:
+                print('Invalid Command.')
+        elif "V" in task:
+            print('Technicians:',technician_id)
+            print('Engineers:',engineer_id)
+            print('Admins:',admin_name)
+        else:
+            print('Invalid Command.')
+    elif "S" in cmd:
+        print('Service Manager-')
+        task=input('Press A to add new service & V to view services.')
+        if "A" in task:
+            service_add()
+        elif "V" in task:
+            rn=input('Please enter the Aircraft Registration Number:')
+            if rn not in register_num:
+                print('Registration Number not found.')
+            else:
+                im=register_num.index(rn)
+                print('Registration Number:',register_num[im])
+                print('Manufacturer Type:',manufactur_type[im])
+                print('Manufacturer Name:',manufactur_name[im])
+                print('ATD-IATA:',atd_iata[im])
+                print('ATD-ICAQ:',atd_icaq[im])
+                print('Previous Service Details: Data Not Available')
+                print('Date of Service: Data Not Available')
+                print('Next Service: Data Not Available')
+                print('Type of Check: Data Not Available')
+                print('Engineer in charge: Data Not Available')
+                print('Assigned Technician: Data Not Available')
+        else:
+             print('Invalid Command.')
+print('Thank you for using our services.')             
         
         
